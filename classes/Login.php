9f4dcc8e24bdcab4d56d020e08f9cc8708c77ddb
ramiromtz <ramiromtz770@gmail.com>
2021-02-05 18:48:13 -0600

    version 1

---
+++ classes/Login.php
@@ -0,0 +1,102 @@
+<?php
+
+class Login
+{
+    /**
+     * @var object The database connection
+     */
+    private $db_connection = null;
+    /**
+     * @var array Collection of error messages
+     */
+    public $errors = array();
+    /**
+     * @var array Collection of success / neutral messages
+     */
+    public $messages = array();
+
+    
+    public function __construct()
+    {
+        session_start();
+
+        if (isset($_GET["logout"])) {
+            $this->doLogout();
+        }
+        elseif (isset($_POST["login"])) {
+            $this->dologinWithPostData();
+        }
+    }
+
+    
+    private function dologinWithPostData()
+    {
+        
+        if (empty($_POST['user_name'])) {
+            $this->errors[] = "Username field was empty.";
+        } elseif (empty($_POST['user_password'])) {
+            $this->errors[] = "Password field was empty.";
+        } elseif (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
+
+            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+            if (!$this->db_connection->set_charset("utf8")) {
+                $this->errors[] = $this->db_connection->error;
+            }
+
+            if (!$this->db_connection->connect_errno) {
+
+                $user_name = $this->db_connection->real_escape_string($_POST['user_name']);
+
+                
+                $sql = "SELECT user_id, user_name, firstname, user_email, user_password_hash
+                        FROM users
+                        WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
+                $result_of_login_check = $this->db_connection->query($sql);
+
+                if ($result_of_login_check->num_rows == 1) {
+
+                    $result_row = $result_of_login_check->fetch_object();
+
+                    
+                    if (password_verify($_POST['user_password'], $result_row->user_password_hash)) {
+
+                        $_SESSION['user_id'] = $result_row->user_id;
+						$_SESSION['firstname'] = $result_row->firstname;
+						$_SESSION['user_name'] = $result_row->user_name;
+                        $_SESSION['user_email'] = $result_row->user_email;
+                        $_SESSION['user_login_status'] = 1;
+
+                    } else {
+                        $this->errors[] = "Usuario y/o contraseña no coinciden.";
+                    }
+                } else {
+                    $this->errors[] = "Usuario y/o contraseña no coinciden.";
+                }
+            } else {
+                $this->errors[] = "Problema de conexión de base de datos.";
+            }
+        }
+    }
+
+    
+    public function doLogout()
+    {
+        $_SESSION = array();
+        session_destroy();
+        $this->messages[] = "Has sido desconectado.";
+
+    }
+
+    /**
+     * simply return the current state of the user's login
+     * @return boolean user's login status
+     */
+    public function isUserLoggedIn()
+    {
+        if (isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] == 1) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="navbar navbar-default navbar-fixed-bottom" >
+    <div class="container text-center">
+      <p>UTVCO - TECNOLOGÍAS DE LA INFORMACIÓN</p>
+      <small>GRUPO TI-502</small>
+   </div>
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
